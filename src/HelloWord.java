f124ea68a54ac493da73227b3d3c77ce441471eb
kameugne <rkameugne@gmail.com>
2021-03-16 09:33:38 +0100

    add triangle class

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -8,5 +8,8 @@
 
          System.out.println(rec.perimetre() + ";" + rec.surface());
        //  rec.PrintRectangle();
+         System.out.println("voici de nouvelle modifications que j'apporte a votre projet");
+         Triangle tri = new Triangle("triangle", 20.0, 15.5, 35.5);
+         System.out.println("Perimetre du triangle est : " + tri.perimetre());
      }
  }
