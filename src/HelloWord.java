600e768e51af63a740ea1f6a2973ad6dd2ffcbae
mireilleyonka <mireille_youcheu@yahoo.fr>
2021-03-16 09:46:06 +0100

    MESSAGE

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -11,5 +11,6 @@
          System.out.println("voici de nouvelle modifications que j'apporte a votre projet");
          Triangle tri = new Triangle("triangle", 20.0, 15.5, 35.5);
          System.out.println("Perimetre du triangle est : " + tri.perimetre());
+         System.out.println("bien dormi?");
      }
  }
